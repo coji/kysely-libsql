badd919507e37241f323015bcf8c5b5889a901ba
coji <coji@techtalk.jp>
2024-03-15 23:47:55 +0900

    Update target and formats in tsconfig.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
       entry: "src/index.ts",
-      formats: ["es"],
+      formats: ["es", "cjs"],
+      fileName: "index",
     },
   },
+  plugins: [
+    dts({ exclude: ["vite.config.ts", "node_modules/**"], rollupTypes: true }),
+  ],
 });
