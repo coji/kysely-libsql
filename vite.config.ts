8079f1ef6c8f5a0d8c229f725d52a66d15cf603b
coji <coji@techtalk.jp>
2024-03-16 17:35:05 +0900

    chore: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.ts",
-      formats: ["es", "cjs"],
-      fileName: "index",
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: 'index',
     },
   },
   plugins: [
-    dts({ exclude: ["vite.config.ts", "node_modules/**"], rollupTypes: true }),
+    dts({ exclude: ['vite.config.ts', 'node_modules/**'], rollupTypes: true }),
   ],
-});
+})
